d9cc295919e0fa3238104dd64cd564582d62c62d
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2019-08-31 09:15:50 -0300

    Filtering data with Spark

--- big-data-spark-student/src/rdd/airport/AirportUSA.java
+++ big-data-spark-student/src/rdd/airport/AirportUSA.java
@@ -12,13 +12,26 @@ public class AirportUSA {
     // a regular expression which matches commas but not commas within double quotations
     public static final String COMMA_DELIMITER = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Logger.getLogger("org").setLevel(Level.ERROR);
+
         // habilita o uso de 2 threads
         SparkConf conf = new SparkConf().setAppName("airport").setMaster("local[1]");
+
         // cria o contexto da aplicacao
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        // leitura do arquivo
+        JavaRDD<String> aeroportos = sc.textFile("in/airports.text");
 
+        // indice do pais em cada linha
+        int ixPais = 3;
+
+        // Filtrando aeroportos no Brasil
+        JavaRDD<String> aeroportosBrazil =
+                aeroportos.filter(airport -> airport.split(COMMA_DELIMITER)[ixPais].equalsIgnoreCase("\"Brazil\""));
+
+        // Salvando em arquivo
+        aeroportosBrazil.saveAsTextFile("output/airportsBr.text");
     }
 }
