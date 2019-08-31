721fc0d2d1ed14515a07993e9aaeceb6d0731206
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2019-08-31 10:22:20 -0300

    Union in Spark

--- big-data-spark-student/src/rdd/nasa/Union.java
+++ big-data-spark-student/src/rdd/nasa/Union.java
@@ -7,15 +7,24 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class Union {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Logger.getLogger("org").setLevel(Level.ERROR);
+
         // habilita o uso de 2 threads
         SparkConf conf = new SparkConf().setAppName("wordCount").setMaster("local[1]");
+
         // cria o contexto da aplicacao
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        // carregando arquivos originais
+        JavaRDD<String> nasaA = sc.textFile("in/nasa_19950701.tsv");
+        JavaRDD<String> nasaB = sc.textFile("in/nasa_19950801.tsv");
 
+        // removendo o header do arquivo B
+        String cabecalho = nasaB.first();
+        nasaB = nasaB.filter(line -> !line.equalsIgnoreCase(cabecalho));
 
+        JavaRDD<String> uniao = nasaA.union(nasaB);
+        uniao.saveAsTextFile("output/nasacombinado.tsv");
     }
-
 }
